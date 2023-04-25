8c292058e80edc61576d9648086d27e8ad65081e
Pol Guixe <pol@origen.studio>
2023-04-25 08:20:24 +0200

    feat: moved logic to another file

---
+++ assets/shipping-time.js
@@ -0,0 +1,123 @@
+// Shipping Time Countdown
+if (!customElements.get("shipping-time")) {
+  customElements.define(
+    "shipping-time",
+    class ShippingTimeUpdate extends HTMLElement {
+      constructor() {
+        console.log("====================================");
+        console.log("ShippingTimeUpdate");
+        console.log("====================================");
+        super();
+        this.fetchAvailability(this.dataset.variantId);
+        this.errorHtml =
+          this.querySelector("template").content.firstElementChild.cloneNode(
+            true
+          );
+        //   this.onClickRefreshList = this.onClickRefreshList.bind(this);
+      }
+
+      fetchAvailability(variantId) {
+        console.log("====================================");
+        console.log("fetchAvailability", variantId);
+        console.log("====================================");
+
+        let rootUrl = this.dataset.rootUrl;
+        if (!rootUrl.endsWith("/")) {
+          rootUrl = rootUrl + "/";
+        }
+        const variantSectionUrl = `${rootUrl}variants/${variantId}/?section_id=pickup-availability`;
+        console.log("====================================");
+        console.log("variantSectionUrl", variantSectionUrl);
+        console.log("====================================");
+
+        fetch(variantSectionUrl)
+          .then(response => {
+            return response.text();            
+          }).then(text => {
+            console.log('====================================');
+            console.log('text', text);
+            console.log('====================================');
+          //   const sectionInnerHTML = new DOMParser()
+          //     .parseFromString(text, 'text/html')
+          //     .querySelector('.shopify-section');
+          //   this.updateShippingText(sectionInnerHTML);
+          })
+          .catch(e => {
+            console.warn('Error fetching pickup availability', e);
+          });
+        this.updateShippingText(variantId);
+      }
+
+      updateShippingText(text) {
+        console.log("====================================");
+        console.log("updateShippingText", text);
+        console.log("====================================");
+      }
+    }
+  );
+}
+
+// function updateShippingTime(){
+// var now = new Date();
+// var day = now.getDay();
+// var end;
+// var diffInHours;
+// var diffInMinutes;
+
+// var textValue = '{{ 'products.product.shipping_time' | t }}'; // "Default text"
+// var shippingTimeCountdown = document.getElementById('shipping-time-countdown');
+
+// var store_availabilities_available = {{ product.selected_or_first_available_variant.store_availabilities | where: "available", true | json }};
+// console.log(store_availabilities_available);
+
+// if (day == 5 && now.getHours() >= 15 || day == 6 || day == 0) {
+
+//   if(store_availabilities_available.length == 0){
+
+//     textValue = '{{ 'products.product.shipping_time_warehouse_weekend' | t }}';
+//     shippingTimeCountdown.innerHTML = 'Thuesday';
+
+//   } else {
+
+//     textValue = '{{ 'products.product.shipping_time_monday' | t }}';
+//     shippingTimeCountdown.innerHTML = 'Monday';
+
+//   }
+
+// } else {
+
+//   if(store_availabilities_available.length == 0){
+
+//     textValue = '{{ 'products.product.shipping_time_warehouse' | t }}';
+//     shippingTimeCountdown.innerHTML = '48-72HRS';
+
+//   } else {
+
+//     if (day >= 1 && day <= 5 && now.getHours() < 15) {
+//       end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0, 0, 0);
+//     } else {
+//       end = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 15, 0, 0, 0);
+//       textValue = '{{ 'products.product.shipping_time_tomorrow' | t }}';
+//     }
+
+//     var timeleft = end.getTime() - now.getTime();
+
+//     diffInHours = Math.floor(timeleft / (1000 * 60 * 60));
+//     diffInMinutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+
+//     shippingTimeCountdown.innerHTML = diffInHours + 'HRS ' + diffInMinutes + 'MIN ';
+//   }
+// }
+
+// var shippingTimeText = document.getElementById('shipping-time-text');
+//                 shippingTimeText.innerHTML = textValue;
+// }
+
+// updateShippingTime();
+
+// console.log(document.getElementsByTagName('variant-radios'));
+
+// document.getElementsByTagName('variant-radios')[0].addEventListener('change', function (event) {
+//     console.log('changed');
+//     // updateShippingTime();
+// });
